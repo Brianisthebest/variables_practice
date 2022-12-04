fec783a3ebe072bd6a2821615d679467e3ed8a33
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2022-12-03 17:44:03 -0700

    Add floats

--- variables.rb
+++ variables.rb
@@ -3,8 +3,13 @@ first_name = "Brian"
 last_name = "Guthrie"
 email = "helloworld@gmail.com"
 
-#Intergers
+# Intergers
 age = 35
 fingers = 10
 wifes_age = 32
 employee_number = 150
+
+# Floats
+amount_paid = 800.50
+amount_due = 1234.56
+remaining_balance = 6543.21
